0bd031dec9b47630cddb3af6ada2965540af5af8
ihor <krohmal.igor777@gmail.com>
2020-04-26 23:28:10 +0300

    added psvm

--- src/main/java/World.java
+++ src/main/java/World.java
@@ -1,10 +1,5 @@
 public class World {
     public static void main(String[] args) {
         System.out.println("Hello World!!");
-        sum();
-    }
-
-    public static void sum() {
-        System.out.println(1+1);
     }
 }
