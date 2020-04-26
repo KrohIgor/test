df656ef57905b9f6d682e01b0b59470bf9c9042b
ihor <krohmal.igor777@gmail.com>
2020-04-26 23:00:39 +0300

    refactored class Test and added print()

---
+++ src/main/java/Hello.java
@@ -0,0 +1,10 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello!!");
+        print();
+    }
+
+    public static void print() {
+        System.out.println("World!!!");
+    }
+}
