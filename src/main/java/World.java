d81278541b6e246e3d66911203e4ef1cdec46e43
ihor <krohmal.igor777@gmail.com>
2020-04-26 23:19:39 +0300

    added sum

--- src/main/java/World.java
+++ src/main/java/World.java
@@ -1,5 +1,10 @@
 public class World {
     public static void main(String[] args) {
         System.out.println("Hello World!!");
+        sum();
+    }
+
+    public static void sum() {
+        System.out.println(1+1);
     }
 }
